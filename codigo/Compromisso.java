ccb1f400a7008fff0829f98e47f5bdcd4f434b67
Rafaella <rafacssacrament@gmail.com>
2022-08-20 10:45:21 -0300

    criação da classe Compromisso diagrama:3versao

--- codigo/Compromisso.java
+++ codigo/Compromisso.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
 public class Compromisso{
-    
+    private int quantidade;    
+    private int intervaloDias;
+    private String dataPrimeiro;
+    private String data;
+
+    public int getQuantidade(){
+        return quantidade 
+    }
+    public int intervaloDias(){
+        return quantidade 
+    }
+    public String dataPrimeiro(){
+        return quantidade 
+    }
+    public String data(){
+        return quantidade 
+    }
+
+    public void criarCompromissos(){
+
+    }
+    public String calcularDatas(){
+        return '01-01-2000'
+    }
 }
