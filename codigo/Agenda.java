473d08cf0b289e1094b0ccd3238e6ac4d66ca5a6
Rafaella Cristina <rafacssacrament@gmail.com>
2022-08-23 18:56:04 -0300

    criando a classe agenda

---
+++ codigo/Agenda.java
@@ -0,0 +1,10 @@
+public class Agenda{
+    private Compromisso compromisso = new Compromisso();
+    
+    public void relatorioCompromisso(){
+        
+    }
+    public void marcarCompromisso(int quantidadeDias,int intervaloDias, Data dataInicial, String nomeCompromisso){
+
+    }
+}
