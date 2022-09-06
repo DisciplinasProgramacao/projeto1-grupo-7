5559134e51ba9aece2ba3e509a8440313eb69b43
Rafaella Cristina De Sousa Sacramento <69471056+RafaellaCristinaCSS@users.noreply.github.com>
2022-09-06 18:53:38 -0300

    documentando metodos

--- codigo/Compromisso.java
+++ codigo/Compromisso.java
@@ -1,17 +1,30 @@
-import java.util.Scanner;
 public class Compromisso {
   private String nomeCompromisso;
   private String dataCompromisso;
 
+  /**
+   * Construtor da Classe, passando todos os atribuos
+   * @param nomeCompromisso O nome ou a descricao do comprimisso a ser agendado
+   * @param dataCompromisso A data que ocorrera o comprimisso
+   * @return retorna true, caso a classe tenha sido criada
+   */
   public boolean criarCompromissos(String nomeCompromisso,String dataCompromisso) {
+    Data data = new Data();
+    data.dataValida();
     this.nomeCompromisso = nomeCompromisso;
     this.dataCompromisso = dataCompromisso;
     
     return true;
   }
+
+  /**
+   * Construtor da Classe, passando apenas o nome ou a descricao 
+   * @param nomeCompromisso O nome ou a descricao do comprimisso a ser agendado
+   * @return retorna true, para informar que a classe foi criada
+   */
   public boolean criarCompromissos(String nomeCompromisso) {
     this.nomeCompromisso = nomeCompromisso;
-    this.dataCompromisso = "Nao definida";
+    this.dataCompromisso = "Nao definida"; 
  
     return true;
   }
