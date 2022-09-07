d302236cf6632870242fc4284d712132b4759e61
Rafaella Cristina <rafacssacrament@gmail.com>
2022-09-07 15:51:13 -0300

    documentando metodos

--- codigo/Data.java
+++ codigo/Data.java
@@ -8,16 +8,24 @@ public class Data {
 
 	public static void main(String[] args) {
 	Data data = new Data();
-	boolean resposta = data.dataValida(12, 10, 3);
+	boolean resposta = data.dataValida(2022, 10, 3);
 	System.out.println(resposta);
-	String resposta2 = data.obterDataFormatada();
+	String resposta2= data.obterDataFormatada();
 	System.out.println(resposta2);
 	}
 	
+	/**
+	 * Metodo criado para formatar a data para String
+	 * @return retorna a data formatada
+	 */
 	public String obterDataFormatada() {
 		return String.format("%d/%d/%d", dia, mes, ano);
 	}
-
+	/**
+	 * Metodo criado para analizar se o ano é bissexto
+	 * @param ano é necessario saber o ano para fazer a devida conta
+	 * @return retorna true, caso o ano seja bissexto e false caso nao seja
+	 */
 	public boolean anoBissexto(int ano) {
 		if (ano % 4 == 0) {
 			return true;
@@ -25,7 +33,13 @@ public class Data {
 			return false;
 		}
 	}
-
+	/**
+	 * Metodo criado para saber se a data é valida
+	 * @param ano o metodo so aceita anos a partir de 2022 (ja que os compromissos so aconteceram no futuro)
+	 * @param mes o metodo atua como no calendario, meses de 1 a 12 
+	 * @param dia os dias sao analizados de acordo com o ano (se é ou nao bissexto) e o mes. 
+	 * @return retorna false, caso os valores passados nao formem uma data valida e caso contrario, true
+	 */
 	public boolean dataValida(int ano, int mes, int dia) {
 		int[] maxDiaDoMes = { 31, 28, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
