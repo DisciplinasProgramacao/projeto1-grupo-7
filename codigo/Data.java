ee882aa9f524928b14d082855b28ce76408e89e2
Rafaella Cristina <rafacssacrament@gmail.com>
2022-09-07 16:14:28 -0300

    complementando comentarios e criando dataValida que recebe uma string

--- codigo/Data.java
+++ codigo/Data.java
@@ -7,23 +7,27 @@ public class Data {
 	int ano;
 
 	public static void main(String[] args) {
-	Data data = new Data();
-	boolean resposta = data.dataValida(2022, 10, 3);
-	System.out.println(resposta);
-	String resposta2= data.obterDataFormatada();
-	System.out.println(resposta2);
+		Data data = new Data();
+		boolean resposta = data.dataValida(2022, 10, 3);
+		System.out.println(resposta);
+		String resposta2 = data.obterDataFormatada();
+		System.out.println(resposta2);
 	}
-	
+
 	/**
 	 * Metodo criado para formatar a data para String
+	 * 
 	 * @return retorna a data formatada
 	 */
 	public String obterDataFormatada() {
 		return String.format("%d/%d/%d", dia, mes, ano);
 	}
+
 	/**
-	 * Metodo criado para analizar se o ano é bissexto ou seja, Para ser bissexto, o ano deve ser: 
+	 * Metodo criado para analizar se o ano é bissexto ou seja, Para ser bissexto, o
+	 * ano deve ser:
 	 * Divisível por 4 e nao pode ser divisível por 100.
+	 * 
 	 * @param ano é necessario saber o ano para fazer a devida conta
 	 * @return retorna true, caso o ano seja bissexto e false caso nao seja
 	 */
@@ -33,14 +37,18 @@ public class Data {
 		} else {
 			return false;
 		}
-		
 	}
+
 	/**
 	 * Metodo criado para saber se a data é valida
-	 * @param ano o metodo so aceita anos a partir de 2022 (ja que os compromissos so aconteceram no futuro)
-	 * @param mes o metodo atua como no calendario, meses de 1 a 12 
-	 * @param dia os dias sao analizados de acordo com o ano (se é ou nao bissexto) e o mes. 
-	 * @return retorna false, caso os valores passados nao formem uma data valida e caso contrario, true
+	 * 
+	 * @param ano o metodo so aceita anos a partir de 2022 (ja que os compromissos
+	 *            so aconteceram no futuro)
+	 * @param mes o metodo atua como no calendario, meses de 1 a 12
+	 * @param dia os dias sao analizados de acordo com o ano (se é ou nao bissexto)
+	 *            e o mes.
+	 * @return retorna false, caso os valores passados nao formem uma data valida e
+	 *         caso contrario, true
 	 */
 	public boolean dataValida(int ano, int mes, int dia) {
 		int[] maxDiaDoMes = { 31, 28, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
@@ -55,11 +63,14 @@ public class Data {
 				return false;
 		} else if (dia > maxDiaDoMes[mes - 1] || dia < 0)
 			return false;
-			else
-			this.dia = dia; 
-			this.mes = mes; 
-			this.ano = ano; 
-			return true;
+		else
+			this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+		return true;
 	}
 
+	public boolean dataValida(String data) {
+		return true;
+	}
 }
